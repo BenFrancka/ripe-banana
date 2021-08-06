573a6a77a64fa3133ddd0d14e4f1c731f1466ae4
Brianna Bias <brianna.bias@gmail.com>
2021-08-05 17:44:56 -0700

    get all test written

--- __tests__/studio.test.js
+++ __tests__/studio.test.js
@@ -1,6 +1,7 @@
 import database from '../lib/utils/database.js';
 import request from 'supertest';
 import app from '../lib/app.js';
+import Studio from '../lib/models/Studio.js';
 // import Studio from '../lib/models/Studio';
 
 describe('Studio routes', () => {
@@ -8,8 +9,48 @@ describe('Studio routes', () => {
     return database.sync({ force: true });
   });
   it('creates a studio via POST', async () => {
-    const result = await request(app).post('/api/v1/studios').send({ name: 'Warner Brothers', city: 'Burbank', state: 'California', country: 'USA' });
-    
-    expect(result.body).toEqual({ id: 1, name: 'Warner Brothers', city: 'Burbank', state: 'California', country: 'USA' });
+    const result = await request(app)
+      .post('/api/v1/studios')
+      .send({
+        name: 'Warner Brothers',
+        city: 'Burbank',
+        state: 'California',
+        country: 'USA',
+      });
+
+    expect(result.body).toEqual({
+      id: 1,
+      name: 'Warner Brothers',
+      city: 'Burbank',
+      state: 'California',
+      country: 'USA',
+    });
+  });
+  it('gets all studios via GET', async () => {
+    const studio1 = {
+      name: 'Warner Brothers',
+      city: 'Burbank',
+      state: 'California',
+      country: 'USA',
+    };
+    const studio2 = {
+      name: 'Lionsgate',
+      city: 'Santa Monica',
+      state: 'California',
+      country: 'USA',
+    };
+    await Studio.bulkCreate([studio1, studio2]);
+    const res = await request(app).get('/api/v1/studios');
+
+    expect(res.body).toEqual([
+      {
+        id: 1,
+        ...studio1,
+      },
+      {
+        id: 2,
+        ...studio2,
+      },
+    ]);
   });
 });
