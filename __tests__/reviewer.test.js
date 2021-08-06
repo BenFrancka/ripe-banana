b99e0bfbcce38611fd3b0bd71d84a5775a27f912
DJ Drakos <dianajodrakos@gmail.com>
2021-08-06 10:48:03 -0700

    created failing test

--- __tests__/reviewer.test.js
+++ __tests__/reviewer.test.js
@@ -4,5 +4,23 @@ import app from '../lib/app.js';
 import Reviewer from '../lib/models/Reviewer.js';
 
 describe('Reviewer routes', () => {
-  
+  beforeEach(() => {
+    return database.sync({ force: true });
+  });
+
+  it('creates a reviewer via POST', async () => {
+    const res = await request(app)
+      .post('/api/v1/reviewers')
+      .send({
+        name: 'Bob',
+        company: 'Rotten Tomatoes'
+      });
+
+    expect(res.body).toEqual({ 
+      id: 1,
+      name: 'Bob',
+      company: 'Rotten Tomatoes'
+    });
+
+  });
 });
