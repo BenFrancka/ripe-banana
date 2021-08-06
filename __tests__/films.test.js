26a79e595803b4541d95e7fd30f0f81d0a53f844
BenFrancka <benfrancka@gmail.com>
2021-08-06 11:52:19 -0700

    adds initial test for Post route to films.test.js

---
+++ __tests__/films.test.js
@@ -0,0 +1,32 @@
+import database from '../lib/utils/database.js';
+import request from 'supertest';
+import app from '../lib/app.js';
+import Film from '../lib/models/Film.js';
+import Studio from '../lib/models/Studio.js';
+
+describe('Film routes', () => {
+  beforeEach(() => {
+    return database.sync({ force: true });
+  });
+
+  it('creates a film with POST', async () => {
+    const studio = await Studio.create({
+      name: 'Warner Brothers',
+      city: 'Burbank',
+      state: 'California',
+      country: 'USA',
+    });
+    const res = await request(app).post('/api/v1/films').send({
+      title: 'Anaconda',
+      studio: studio.id,
+      released: 1997,
+    });
+
+    expect(res.body).toEqual({
+      id: 1,
+      title: 'Anaconda',
+      studio: studio.id,
+      released: 1997,
+    });
+  });
+});
