627146273853932a99795fe07c1b6d7fd71dc18c
Dimitra Weinstein <drenee.weinstein@gmail.com>
2021-08-06 09:57:58 -0700

    created get list of actors test --red

--- __tests__/actor.test.js
+++ __tests__/actor.test.js
@@ -1,6 +1,7 @@
 import database from '../lib/utils/database.js';
 import request from 'supertest';
 import app from '../lib/app.js';
+import Actor from '../lib/models/Actor.js';
 
 describe('Actor routes', () => {
   beforeEach(() => {
@@ -16,5 +17,32 @@ describe('Actor routes', () => {
       dob: '1972-08-15',
       pob: 'Berkeley, CA'
     });
+  });
+    
+  it('gets list of actors via GET', async () => {
+    const actor1 = {
+      name: 'Ben Affleck',
+      dob: '1972-08-15',
+      pob: 'Berkeley, CA'
+    };
+      
+    const actor2 = {
+      name: 'Denzel Washington',
+      dob: '1954-12-28',
+      pob: 'Mount Vernon, NY'
+    };
+      
+    await Actor.bulkCreate([actor1, actor2]);
+      
+    const res = await request(app).get('/api/v1/actors');
+      
+    expect(res.body).toEqual([{
+      id: 1,
+      ...actor1
+    }
+    , {
+      id: 2,
+      ...actor2
+    }]);
   });
 });
