f1dbfc2549f16508ed5aa1ad06637b035f6cb3f9
BenFrancka <benfrancka@gmail.com>
2021-08-06 14:59:40 -0700

    adds initial test for get all films route to films.test.js

--- __tests__/films.test.js
+++ __tests__/films.test.js
@@ -1,7 +1,7 @@
 import database from '../lib/utils/database.js';
 import request from 'supertest';
 import app from '../lib/app.js';
-//import Film from '../lib/models/Film.js';
+import Film from '../lib/models/Film.js';
 import Studio from '../lib/models/Studio.js';
 
 describe('Film routes', () => {
@@ -28,5 +28,38 @@ describe('Film routes', () => {
       studio: `${studio.id}`,
       released: 1997,
     });
+  });
+
+  it('gets all films from the database with GET', async () => {
+    const studio = await Studio.create({
+      name: 'Warner Brothers',
+      city: 'Burbank',
+      state: 'California',
+      country: 'USA',
+    });
+    const film1 = {
+      title: 'Anaconda',
+      studio: studio.id,
+      released: 1997,
+    };
+    const film2 = {
+      title: 'Space Jam',
+      studio: studio.id,
+      released: 1996,
+    };
+
+    await Film.bulkCreate([film1, film2]);
+    const res = await request(app).get('/api/v1/films');
+
+    expect(res.body).toEqual([
+      {
+        id: 1,
+        ...film1,
+      },
+      {
+        id: 2,
+        ...film2,
+      },
+    ]);
   });
 });
