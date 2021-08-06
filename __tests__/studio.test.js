7077ef5988409674611c8ed43e43d44f047a68ca
Brianna Bias <brianna.bias@gmail.com>
2021-08-05 17:37:33 -0700

    post route and passing test

--- __tests__/studio.test.js
+++ __tests__/studio.test.js
@@ -1,14 +1,15 @@
 import database from '../lib/utils/database.js';
 import request from 'supertest';
 import app from '../lib/app.js';
-import Studio from '../lib/models/Studio';
+// import Studio from '../lib/models/Studio';
 
 describe('Studio routes', () => {
   beforeEach(() => {
     return database.sync({ force: true });
   });
   it('creates a studio via POST', async () => {
-    const result = await request(app).post('/api/v1/studios')).send({ name: 'Warner Brothers', city: 'Burbank', state: 'California', country: 'USA' });
+    const result = await request(app).post('/api/v1/studios').send({ name: 'Warner Brothers', city: 'Burbank', state: 'California', country: 'USA' });
     
     expect(result.body).toEqual({ id: 1, name: 'Warner Brothers', city: 'Burbank', state: 'California', country: 'USA' });
   });
+});
