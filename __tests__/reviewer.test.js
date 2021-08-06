af819cb43c5986b88d620bd42e0b75aa9d2d1e8a
DJ Drakos <dianajodrakos@gmail.com>
2021-08-06 10:42:34 -0700

    created blank modules with imports

---
+++ __tests__/reviewer.test.js
@@ -0,0 +1,8 @@
+import database from '../lib/utils/database.js';
+import request from 'supertest';
+import app from '../lib/app.js';
+import Reviewer from '../lib/models/Reviewer.js';
+
+describe('Reviewer routes', () => {
+  
+});
