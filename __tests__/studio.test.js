36262853690caed59acc108f697f90c47c1cadf4
Brianna Bias <brianna.bias@gmail.com>
2021-08-05 17:19:14 -0700

    post test written

--- __tests__/studio.test.js
+++ __tests__/studio.test.js
@@ -7,5 +7,9 @@ describe('Studio routes', () => {
   beforeEach(() => {
     return database.sync({ force: true });
   });
-  
+  it('creates a studio via POST', async () => {
+    const result = await request(app).post('/api/v1/studios')).send({ name: 'Warner Brothers', city: 'Burbank', state: 'California', country: 'USA' });
+    
+    expect(result.body).toEqual({ id: 1, name: 'Warner Brothers', city: 'Burbank', state: 'California', country: 'USA' });
+  });
 });
