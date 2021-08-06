a6668f8acc76335e38a3f81626c591433ad46d2b
Dimitra Weinstein <drenee.weinstein@gmail.com>
2021-08-06 09:22:55 -0700

    create actor via POST test created

---
+++ __tests__/actor.test.js
@@ -0,0 +1,20 @@
+import database from '../lib/utils/database.js';
+import request from 'supertest';
+import app from '../lib/app.js';
+
+describe('Actor routes', () => {
+  beforeEach(() => {
+    return database.sync({ force: true });
+  });
+  it('creates an actor via POST', async () => {
+    const res = await request(app).post('/api/v1/actors')
+      .send({ name: 'Ben Affleck', dob: '1972-08-15', pob: 'Berkley, CA' });
+      
+    expect(res.body).toEqual({
+      id: 1,
+      name: 'Ben Affleck',
+      dob: '1972-08-15',
+      pob: 'Berkely, CA'
+    });
+  });
+});
