79a12b0e55ccf147823a3bac46da9c982f3e6310
Brianna Bias <brianna.bias@gmail.com>
2021-08-09 10:55:54 -0700

    films get by id test written

--- __tests__/films.test.js
+++ __tests__/films.test.js
@@ -3,6 +3,9 @@ import request from 'supertest';
 import app from '../lib/app.js';
 import Film from '../lib/models/Film.js';
 import Studio from '../lib/models/Studio.js';
+import Actor from '../lib/models/Actor';
+import Review from '../lib/models/Review';
+import Reviewer from '../lib/models/Reviewer';
 
 describe('Film routes', () => {
   beforeEach(() => {
@@ -56,17 +59,17 @@ describe('Film routes', () => {
         id: 1,
         Studio: {
           id: 1,
-          name: 'Warner Brothers'
+          name: 'Warner Brothers',
         },
         title: 'Anaconda',
         released: 1997,
       },
-      
+
       {
         id: 2,
         Studio: {
           id: 1,
-          name: 'Warner Brothers'
+          name: 'Warner Brothers',
         },
         title: 'Space Jam',
         released: 1996,
@@ -74,7 +77,67 @@ describe('Film routes', () => {
     ]);
   });
 
-  // it('gets a film by id with GET, includes reviews, studio, and cast/actor', async () => {
-      
-  // }
+  it('gets a film by id with GET, includes reviews, studio, and cast/actor', async () => {
+    const studio = await Studio.create({
+      name: 'Warner Brothers',
+      city: 'Burbank',
+      state: 'California',
+      country: 'USA',
+    });
+
+    const actor = await Actor.create({
+      name: 'Denzel Washington',
+      dob: '1954-12-28',
+      pob: 'Mount Vernon, NY',
+    });
+
+    const film = await Film.create({
+      title: 'Anaconda',
+      released: 1997,
+    });
+
+    const reviewer = await Reviewer.create({
+      name: 'Bob',
+      company: 'Rotten Tomatoes',
+    });
+
+    const review = await Review.create({
+      rating: 3,
+      review: 'this was fine i guess',
+    });
+
+    await actor.setFilms(film);
+    await film.setStudio(studio);
+    await film.setActors(actor);
+    await review.setReviewer(reviewer);
+    await review.setFilm(film);
+
+    const res = await request(app).get(`/api/v1/films/${film.id}`);
+
+    expect(res.body).toEqual({
+      title: 'Anaconda',
+      released: 1997,
+      studio: {
+        id: studio.id,
+        name: 'Warner Brothers',
+      },
+      cast: [
+        {
+          id: actor.id,
+          name: 'Denzel Washington',
+        },
+      ],
+      reviews: [
+        {
+          id: review.id,
+          rating: 3,
+          review: 'this was fine i guess',
+          reviewer: {
+            id: reviewer.id,
+            name: 'Bob',
+          },
+        },
+      ],
+    });
+  });
 });
